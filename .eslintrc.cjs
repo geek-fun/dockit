232359ed2360e651248fc9c1d28604463e7867d2
seven <zilisheng1996@gmail.com>
2023-07-23 00:02:06 +0800

    re-orgnize project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,31 @@
 module.exports = {
-  // 若项目中有多个子项目，且每个项目都会有.eslintrc，子项目会一直向上查找所有的.eslintrc，直到找到root:true的eslintrc，再将所有的.eslintrc合并
-  root: true,
   env: {
-    // Node.js 全局变量和作用域
     node: true,
     es2022: true,
   },
-  // 继承另一个配置文件的所有特性
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  // 解释器
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  // 插件，向ESLint添加各种扩展，可以定义规则，环境或配置的第三方模块
-  plugins: ['prettier', '@typescript-eslint'],
-  // 规则
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    // 禁止使用 console
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 禁止使用 debugger
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 逗号前面没有空格 后面有空格
-    'comma-spacing': [
-      2,
+    'prettier/prettier': 'error',
+    '@typescript-eslint/ban-types': [
+      'error',
       {
-        before: false,
-        after: true,
+        types: {
+          // for codegen un-ban a type that's banned by default
+          '{}': false,
+        },
+        extendDefaults: true,
       },
     ],
-    'prettier/prettier': 'warn',
   },
+  ignorePatterns: ['dist'],
 };
