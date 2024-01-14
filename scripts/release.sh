bd879d9e9932a7f6091f3886e89ea30d06b47f41
seven <zilisheng1996@gmail.com>
2024-01-14 15:55:39 +0800

    feat: updates get token #3
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,4 +3,4 @@ set -o pipefail
 
 cd "$(dirname "$0")/.." || exit
 
-GITHUB_TOKEN="${{ env.GITHUB_TOKEN }}" npx electron-forge publish
+GITHUB_TOKEN="${{ secrets.GITHUB_TOKEN }}" npx electron-forge publish
