b80437cc788f73c7baccb4217f0e2aa9c5bee820
seven <zilisheng1996@gmail.com>
2024-02-19 22:12:07 +0800

    fix: update config
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
