4572f84a073488685d73b59bed6dbbde3ddf70f0
seven <zilisheng1996@gmail.com>
2024-04-13 00:26:46 +0800

    feat: add chat box frame #35
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- src/layout/components/tool-bar-right.vue
+++ src/layout/components/tool-bar-right.vue
@@ -1,5 +1,40 @@
 <template>
-  <n-card v-if="chatBot.active" class="chat-box-container"> chatbot box </n-card>
+  <n-card
+    v-if="chatBot.active"
+    class="chat-box-container"
+    :title="$t('aside.chatBot')"
+    :segmented="{
+      content: true,
+      footer: 'segmented',
+    }"
+  >
+    <template #header-extra></template>
+    Card Content
+    <template #footer>
+      <div class="message-box">
+        <n-input
+          v-model:value="message"
+          type="textarea"
+          :autosize="{
+            minRows: 3,
+          }"
+          placeholder="Type your message here..."
+        />
+        <n-button-group class="message-action-box">
+          <n-button text>
+            <template #icon> </template>
+          </n-button>
+          <n-button text @click="sendMessage">
+            <template #icon>
+              <n-icon size="26">
+                <send-alt />
+              </n-icon>
+            </template>
+          </n-button>
+        </n-button-group>
+      </div>
+    </template>
+  </n-card>
   <div class="tool-bar-right">
     <the-aside-icon
       v-for="item in smallNavList"
@@ -21,7 +56,8 @@
 
 <script setup lang="ts">
 import { markRaw, ref } from 'vue';
-import { ChatBot } from '@vicons/carbon';
+import { ChatBot, SendAlt } from '@vicons/carbon';
+import TheAsideIcon from './the-aside-icon.vue';
 
 const selectedItemId = ref(-1);
 const chatBot = ref({ active: false });
@@ -39,6 +75,15 @@ const smallNavList = ref([
     name: 'chatBot',
   },
 ]);
+
+const message = ref(''); // to hold the message
+
+const sendMessage = () => {
+  if (message.value.trim() !== '') {
+    console.log(message.value); // replace this with your actual send message logic
+    message.value = ''; // clear the input after sending the message
+  }
+};
 </script>
 
 <style scoped>
@@ -77,5 +122,14 @@ const smallNavList = ref([
 }
 .chat-box-container {
   width: 500px;
+  .message-box {
+    background-color: var(--border-color);
+    .message-action-box {
+      display: flex;
+      justify-content: space-between;
+      height: 20px;
+      padding: 10px;
+    }
+  }
 }
 </style>
