692cb72f42c38697c306c17024cb92565958fc3f
seven <zilisheng1996@gmail.com>
2024-07-07 23:27:28 +0800

    fix: collect & standlize binary name
    Signed-off-by: seven <zilisheng1996@gmail.com>

---
+++ scripts/collect-binaries.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+set -o pipefail
+
+cd "$(dirname "$0")/.." || exit
+
+if [ ! -d artifacts ]; then
+  mkdir artifacts
+fi
+
+VERSION=$(node -p "require('./package.json').version")
+PLATFORM=${PLATFORM:-"macos-latest"}
+
+if [[ $PLATFORM == "windows-latest" ]]; then
+  mv src-tauri/target/x86_64-pc-windows-msvc/release/bundle/nsis/*.exe artifacts/
+fi
+if [[ $PLATFORM == "macos-latest" ]]; then
+  mv src-tauri/target/universal-apple-darwin/release/bundle/dmg/*.dmg artifacts/
+fi
+
+if [[ $PLATFORM == "ubuntu-latest" ]]; then
+  mv src-tauri/target/x86_64-unknown-linux-gnu/release/bundle/deb/*.deb artifacts/DocKit_"${VERSION}"_amd64.deb
+  mv src-tauri/target/x86_64-unknown-linux-gnu/release/bundle/appimage/*.AppImage artifacts/DocKit_"${VERSION}"_amd64.AppImage
+fi
