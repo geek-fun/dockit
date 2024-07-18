a4fc576b6940747f8bb70977d5cc947a66a569ef
seven <zilisheng1996@gmail.com>
2024-07-18 23:08:44 +0800

    [wip] add node status bar
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- src/views/manage/components/node-state.vue
+++ src/views/manage/components/node-state.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="node-list-container">
-    <n-card
-      class="node-item"
-      hoverable
-      v-for="node in established?.rawClusterState?.nodes.instances"
-      :key="node.name"
-      :title="node.name"
-    >
-    </n-card>
+  <n-scrollbar style="max-height: 420px">
+    <div class="node-list-container">
+      <n-card
+        class="node-item"
+        hoverable
+        v-for="node in established?.rawClusterState?.nodes.instances"
+        :key="node.name"
+        :title="node.name"
+      >
+        {{ node.name }}
+      </n-card>
+    </div>
+  </n-scrollbar>
+  <div class="node-state-bar">
+    <div v-for="stateItem in nodeStats" :key="stateItem.key" class="node-state-bar-item">
+      <p>{{ stateItem.key }}</p>
+      <p>{{ stateItem.value }}</p>
+    </div>
   </div>
 </template>
 
@@ -18,7 +27,44 @@ import { storeToRefs } from 'pinia';
 const connectionStore = useConnectionStore();
 const { fetchNodes } = connectionStore;
 const { established } = storeToRefs(connectionStore);
-
+const nodeStats = ref([
+  {
+    key: 'status',
+    value: 'green',
+  },
+  {
+    key: 'alerts',
+    value: '3',
+  },
+  {
+    key: 'ip',
+    value: '21.155.209.72:9300',
+  },
+  {
+    key: 'JVM',
+    value: '43%',
+  },
+  {
+    key: 'disk',
+    value: '1%',
+  },
+  {
+    key: 'docs',
+    value: '970.8k',
+  },
+  {
+    key: 'data',
+    value: '644.4 MB',
+  },
+  {
+    key: 'index',
+    value: 31,
+  },
+  {
+    key: 'shards',
+    value: 31,
+  },
+]);
 fetchNodes();
 </script>
 
@@ -30,8 +76,21 @@ fetchNodes();
   gap: 15px;
   margin-top: 10px;
   cursor: pointer;
+
   .node-item {
-    max-width: 200px;
+    max-width: 300px;
   }
+}
+.node-state-bar {
+  margin: 10px 0;
+  border-radius: 3px;
+  border: 1px solid var(--border-color);
+  background-color: var(--n-color);
+  color: var(--n-text-color);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  text-align: center;
 }
 </style>
