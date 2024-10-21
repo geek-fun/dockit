ba02cbbc83b2d8e98ed88daf3d98633a4d0af467
seven <zilisheng1996@gmail.com>
2024-10-21 21:52:04 +0800

    feat: openfolder
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- src/views/file/components/tool-bar.vue
+++ src/views/file/components/tool-bar.vue
@@ -2,7 +2,7 @@
   <div class="tool-bar-container">
     <n-tooltip trigger="hover" v-for="toolBar in toolBarList">
       <template #trigger>
-        <n-icon size="26" class="tool-bar-item">
+        <n-icon size="26" class="tool-bar-item" @click="handleToolBarAction(toolBar.id)">
           <component :is="toolBar.icon" />
         </n-icon>
       </template>
@@ -12,25 +12,51 @@
 </template>
 
 <script setup lang="ts">
+import { open } from '@tauri-apps/api/dialog';
 import { DocumentAdd, FolderAdd, FolderOpen } from '@vicons/carbon';
+
+enum ToolBarAction {
+  ADD_DOCUMENT = 'ADD_DOCUMENT',
+  ADD_FOLDER = 'ADD_FOLDER',
+  OPEN_FOLDER = 'OPEN_FOLDER',
+}
 
 const toolBarList = [
   {
-    id: 'add-document',
+    id: ToolBarAction.ADD_DOCUMENT,
     icon: DocumentAdd,
     title: 'Add Document',
   },
   {
-    id: 'add-folder',
+    id: ToolBarAction.ADD_FOLDER,
     icon: FolderAdd,
     title: 'Add Folder',
   },
   {
-    id: 'open-folder',
+    id: ToolBarAction.OPEN_FOLDER,
     icon: FolderOpen,
     title: 'Open Folder',
   },
 ];
+
+const handleToolBarAction = async (id: ToolBarAction) => {
+  if (id === ToolBarAction.ADD_DOCUMENT) {
+    console.log('Add Document');
+  } else if (id === ToolBarAction.ADD_FOLDER) {
+    console.log('Add Folder');
+  } else if (id === ToolBarAction.OPEN_FOLDER) {
+    try {
+      const selectedFiles = await open({
+        multiple: false,
+        directory: true,
+      });
+      console.log('Selected files:', selectedFiles);
+    } catch (error) {
+      console.error('Failed to open file dialog:', error);
+    }
+    console.log(id);
+  }
+};
 </script>
 
 <style lang="scss" scoped>
