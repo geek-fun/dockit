81e10a57ec08ecec4d4e75ff7384580803f3546e
Fujun <1064980344@qq.com>
2024-12-26 14:23:47 +0800

    feat: revert main

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <n-config-provider>
-    <n-message-provider>
-      <router-view />
-      <version-detect />
-    </n-message-provider>
-  </n-config-provider>
+  <app-provider>
+    <router-main />
+    <version-detect />
+  </app-provider>
 </template>
 
 <script setup lang="ts">
-import { NConfigProvider, NMessageProvider } from 'naive-ui';
+import AppProvider from './components/AppProvider.vue';
+import RouterMain from './components/RouterMain.vue';
 import VersionDetect from './components/VersionDetect.vue';
 </script>
