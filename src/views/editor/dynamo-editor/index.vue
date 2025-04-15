47cc1684320c5202211333b4eae2b5b0df597ab3
seven <zilisheng1996@gmail.com>
2025-04-15 22:40:34 +0800

    refactor: restructure editor folder
    Signed-off-by: seven <zilisheng1996@gmail.com>

---
+++ src/views/editor/dynamo-editor/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dynamo-editor">
+    <n-tabs type="segment">
+      <n-tab-pane name="query" tab="Query">
+        <n-empty :description="$t('connection.dynamodb.queryComingSoon')" />
+      </n-tab-pane>
+      <n-tab-pane name="tables" tab="Tables">
+        <n-empty :description="$t('connection.dynamodb.tablesComingSoon')" />
+      </n-tab-pane>
+    </n-tabs>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
